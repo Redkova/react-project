22ad786a3c96b5e097b3a5816ead5325af352c20
Sandra Redkova <sandra_09_94@mail.ru>
2026-05-10 20:20:28 +0200

    test: add Spinner component tests

--- rs-react-app/src/components/movies/Spinner.tsx
+++ rs-react-app/src/components/movies/Spinner.tsx
@@ -3,7 +3,7 @@ import { Component, type ReactNode } from 'react';
 class Spinner extends Component {
   render(): ReactNode {
     return (
-      <div className="flex justify-center py-10">
+      <div data-testid="spinner" className="flex justify-center py-10">
         <div className="h-10 w-10 border-4 border-blue-500 border-t-transparent rounded-full animate-spin"></div>
       </div>
     );
