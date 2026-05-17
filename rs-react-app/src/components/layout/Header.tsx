50a100cd04f0e879175ff07d41c8fe49ec980d46
Sandra Redkova <sandra_09_94@mail.ru>
2026-05-18 01:08:02 +0200

    feat: add about page with navigation and author info

--- rs-react-app/src/components/layout/Header.tsx
+++ rs-react-app/src/components/layout/Header.tsx
@@ -1,7 +1,30 @@
+import { Link } from 'react-router';
+
 function Header() {
   return (
-    <header className="w-full py-4 text-center text-xl font-bold bg-white shadow-sm">
-      <h1>Find Your Movie</h1>
+    <header className="w-full bg-white shadow-sm">
+      <div className="max-w-5xl mx-auto flex items-center justify-between px-6 py-4">
+        <div className="w-24" />
+        <Link
+          to="/"
+          className="text-xl font-bold text-center flex-1 transition 
+            hover:scale-[1.2] 
+            hover:text-blue-600"
+        >
+          <h1>Find Your Movie</h1>
+        </Link>
+
+        <nav className="w-24 text-right">
+          <Link
+            to="/about"
+            className=" 
+              hover:text-blue-600
+              transition"
+          >
+            About
+          </Link>
+        </nav>
+      </div>
     </header>
   );
 }
