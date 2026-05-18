a1b02ab5875d6a717c932ffa442aac4c7cf8e196
Sandra Redkova <sandra_09_94@mail.ru>
2026-05-18 14:05:11 +0200

    test: rewrite test for App component

--- rs-react-app/src/main.tsx
+++ rs-react-app/src/main.tsx
@@ -2,14 +2,14 @@ import './style.css';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router';
-import { AppRouter } from './router/AppRouter.tsx';
+import App from './App.tsx';
 import ErrorBoundary from './components/errorBoundary/ErrorBoundary.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <ErrorBoundary>
       <BrowserRouter>
-        <AppRouter />
+        <App />
       </BrowserRouter>
     </ErrorBoundary>
   </StrictMode>
