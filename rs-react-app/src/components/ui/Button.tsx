08910d828f9ef7052d6e8245f2078b67431ad72b
Sandra Redkova <sandra_09_94@mail.ru>
2026-05-22 13:37:50 +0200

    fix: adjust disabled button for pagination

--- rs-react-app/src/components/ui/Button.tsx
+++ rs-react-app/src/components/ui/Button.tsx
@@ -4,6 +4,7 @@ function Button({ children, className = '', disabled, ...props }: ButtonProps) {
   return (
     <button
       {...props}
+      disabled={disabled}
       className={`px-5 py-2 rounded-xl transition
         ${className} ${disabled ? 'cursor-not-allowed' : 'cursor-pointer'}
         ${disabled ? '' : 'hover:bg-blue-600'}`}
