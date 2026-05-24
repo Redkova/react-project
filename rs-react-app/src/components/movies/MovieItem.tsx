42ceaba4a79845723a5de21f595d357a9be0e9e7
Sandra Redkova <sandra_09_94@mail.ru>
2026-05-24 15:52:13 +0200

    feat: implement movie checkbox selection

--- rs-react-app/src/components/movies/MovieItem.tsx
+++ rs-react-app/src/components/movies/MovieItem.tsx
@@ -1,43 +1,65 @@
 import { useState } from 'react';
 import type { OmdbMovie } from '../../api/types';
-import { Link } from 'react-router';
+import { useNavigate } from 'react-router';
 import { useMovieParams } from '../../hooks/useMovieParams';
+import { useAppDispatch, useAppSelector } from '../../hooks/reduxHooks';
+import { toggleMovieSelection } from '../../store/selectedMoviesSlice';
+import { Checkbox } from '../ui/Checkbox';
 
 interface Props {
   movie: OmdbMovie;
 }
 
 function MovieItem({ movie }: Props) {
+  const navigate = useNavigate();
+  const dispatch = useAppDispatch();
+
   const { search, page } = useMovieParams();
   const [imageError, setImageError] = useState(false);
+
+  const isSelected = useAppSelector((state) =>
+    state.selectedMovies.selectedMovies.some((m) => m.imdbID === movie.imdbID)
+  );
+
   const hasPoster =
     movie.Poster && movie.Poster !== 'N/A' && movie.Poster !== '';
-  // const { search, page } = useMovieParams();
+
   const showPoster = hasPoster && !imageError;
 
+  const handleOpenDetails = () => {
+    navigate(`/?search=${search}&page=${page}&details=${movie.imdbID}`);
+  };
+
+  const handleCheckboxChange = () => {
+    dispatch(toggleMovieSelection(movie));
+  };
+
   return (
-    <Link to={`/?search=${search}&page=${page}&details=${movie.imdbID}`}>
-      <div className="flex justify-between items-center p-4 border rounded-xl bg-gray-50 shadow-sm hover:shadow-md transition">
-        <div className="flex items-center gap-4">
-          {showPoster ? (
-            <img
-              src={movie.Poster}
-              alt={movie.Title}
-              className="w-14 h-20 object-cover rounded-md border"
-              onError={() => setImageError(true)}
-            />
-          ) : (
-            <div className="w-14 h-20 rounded-md bg-linear-to-br from-gray-100 to-gray-300 border border-gray-200 flex flex-col items-center justify-center shadow-sm">
-              <span className="text-[10px] text-gray-600 text-center leading-tight px-1">
-                No image
-              </span>
-            </div>
-          )}
-          <h3 className="text-lg font-semibold">{movie.Title}</h3>
-        </div>
-        <p className="text-gray-700 text-lg font-medium">{movie.Year}</p>
+    <div
+      onClick={handleOpenDetails}
+      className="flex justify-between items-center p-4 border rounded-xl bg-gray-50 shadow-sm hover:shadow-md transition"
+    >
+      <div className="flex items-center gap-4">
+        <Checkbox checked={isSelected} onChange={handleCheckboxChange} />
+
+        {showPoster ? (
+          <img
+            src={movie.Poster}
+            alt={movie.Title}
+            className="w-14 h-20 object-cover rounded-md border"
+            onError={() => setImageError(true)}
+          />
+        ) : (
+          <div className="w-14 h-20 rounded-md bg-linear-to-br from-gray-100 to-gray-300 border border-gray-200 flex flex-col items-center justify-center shadow-sm">
+            <span className="text-[10px] text-gray-600 text-center leading-tight px-1">
+              No image
+            </span>
+          </div>
+        )}
+        <h3 className="text-lg font-semibold">{movie.Title}</h3>
       </div>
-    </Link>
+      <p className="text-gray-700 text-lg font-medium">{movie.Year}</p>
+    </div>
   );
 }
 
--- rs-react-app/src/store/selectedMoviesSlice.ts
+++ rs-react-app/src/store/selectedMoviesSlice.ts
@@ -1,4 +1,4 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, type PayloadAction } from '@reduxjs/toolkit';
 import type { OmdbMovie } from '../api/types';
 
 interface SelectedMoviesState {
@@ -12,7 +12,34 @@ const initialState: SelectedMoviesState = {
 const selectedMoviesSlice = createSlice({
   name: 'selectedMovies',
   initialState,
-  reducers: {},
+  reducers: {
+    toggleMovieSelection(state, action: PayloadAction<OmdbMovie>) {
+      const isMovieSelected = state.selectedMovies.some(
+        (m) => m.imdbID === action.payload.imdbID
+      );
+
+      if (isMovieSelected) {
+        state.selectedMovies = state.selectedMovies.filter(
+          (m) => m.imdbID !== action.payload.imdbID
+        );
+      } else {
+        state.selectedMovies.push(action.payload);
+      }
+    },
+
+    unselectMovie(state, action: PayloadAction<string>) {
+      state.selectedMovies = state.selectedMovies.filter(
+        (m) => m.imdbID !== action.payload
+      );
+    },
+
+    unselectAllMovies(state) {
+      state.selectedMovies = [];
+    },
+  },
 });
+
+export const { toggleMovieSelection, unselectMovie, unselectAllMovies } =
+  selectedMoviesSlice.actions;
 
 export default selectedMoviesSlice.reducer;
