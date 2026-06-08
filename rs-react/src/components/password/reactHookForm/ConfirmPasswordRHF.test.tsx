7d4c3338c2aea913f933e007a26b53eafb092d93
Sandra Redkova <sandra_09_94@mail.ru>
2026-06-08 21:30:40 +0200

    test: add tests for ConfirmPasswordRHF coponent

---
+++ rs-react/src/components/password/reactHookForm/ConfirmPasswordRHF.test.tsx
@@ -0,0 +1,28 @@
+import { describe, it, expect, vi } from 'vitest';
+import { render } from '@testing-library/react';
+import { ConfirmPasswordRHF } from './ConfirmPasswordRHF';
+import type { UseFormRegisterReturn } from 'react-hook-form';
+import type { FormValues } from '../../../validation/validationSchema';
+
+describe('ConfirmPasswordRHF', () => {
+  const registerMock = vi.fn(
+    (name: keyof FormValues): UseFormRegisterReturn => ({
+      onChange: vi.fn(),
+      onBlur: vi.fn(),
+      name,
+      ref: vi.fn(),
+    })
+  );
+
+  it('calls register with correct field name', () => {
+    render(
+      <ConfirmPasswordRHF
+        register={registerMock as unknown as any}
+        passwordValue=''
+        confirmValue=''
+      />
+    );
+
+    expect(registerMock).toHaveBeenCalledWith('confirmPassword');
+  });
+});
